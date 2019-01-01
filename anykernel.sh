fbef80f8472ac697bce8f46c34ab4a92a41b5201
Nageen Chand <nageen523@gmail.com>
2019-01-01 20:52:30 +0530

    Update for slot device - chef

--- anykernel.sh
+++ anykernel.sh
@@ -4,22 +4,22 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=DarkOne Kernel by nageen9211@xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
+device.name1=chef
+device.name2=chef_sprout
+device.name3=
 device.name4=
 device.name5=
 supported.versions=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=auto;
 ramdisk_compression=auto;
 
 
